418936a2d58d054a64b0ded7590f69e957d45bdd
NegativeScript <ala2aldeen913@gmail.com>
2017-11-03 21:21:46 +0300

    Moved feature-envious algorithm from PlayState to PlayerShip

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,9 +1,7 @@
 package;
 
 import backeroids.view.PlayerShip;
-import flixel.FlxG;
 import helix.core.HelixState;
-import flixel.math.FlxPoint;
 using helix.core.HelixSpriteFluentApi;
 
 class PlayState extends HelixState
@@ -21,22 +19,5 @@ class PlayState extends HelixState
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
-		
-		// TODO: refactor into HelixSprite.onKeyPress method
-		if (FlxG.keys.pressed.LEFT || FlxG.keys.pressed.A)
-		{
-			this.playerShip.angle -= 2.5;
-		}
-		else if (FlxG.keys.pressed.RIGHT || FlxG.keys.pressed.D)
-		{
-			this.playerShip.angle += 2.5;
-		}
-
-		this.playerShip.acceleration.set();
-		if (FlxG.keys.pressed.UP || FlxG.keys.pressed.W)
-		{
-			this.playerShip.acceleration.set(0, -90);
-			this.playerShip.acceleration.rotate(FlxPoint.weak(0, 0), this.playerShip.angle);
-		}
 	}
 }
--- source/backeroids/view/PlayerShip.hx
+++ source/backeroids/view/PlayerShip.hx
@@ -1,6 +1,8 @@
 package backeroids.view;
  
 import helix.core.HelixSprite;
+import flixel.FlxG;
+import flixel.math.FlxPoint;
 
 class PlayerShip extends HelixSprite
 {
@@ -12,5 +14,22 @@ class PlayerShip extends HelixSprite
     override public function update(elapsedSeconds:Float):Void
     {
         super.update(elapsedSeconds);
+
+        // TODO: refactor into HelixSprite.onKeyPress method
+		if (FlxG.keys.pressed.LEFT || FlxG.keys.pressed.A)
+		{
+			this.angle -= 2.5;
+		}
+		else if (FlxG.keys.pressed.RIGHT || FlxG.keys.pressed.D)
+		{
+			this.angle += 2.5;
+		}
+
+		this.acceleration.set();
+		if (FlxG.keys.pressed.UP || FlxG.keys.pressed.W)
+		{
+			this.acceleration.set(0, -90);
+			this.acceleration.rotate(FlxPoint.weak(0, 0), this.angle);
+		}
     }
 }
