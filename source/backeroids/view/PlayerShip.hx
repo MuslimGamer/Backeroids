1964488f5abb35b68d5385ed6984fcbc4a15ce04
nightblade9 <nightbladecodes@gmail.com>
2017-11-07 07:35:41 -0500

    Add deceleration Extract constants Simplify key-handling/acceleration

--- source/backeroids/view/PlayerShip.hx
+++ source/backeroids/view/PlayerShip.hx
@@ -11,10 +11,14 @@ using Lambda;
 
 class PlayerShip extends HelixSprite
 {
+    // TODO: should be in config.json
+    private static inline var ROTATION_VELOCITY:Int = 200;
+    private static inline var ACCELERATION:Int = 90;
+    private static inline var DECELERATION_MULTIPLIER:Float = 2;
+
     public function new():Void
     {
         super("assets/images/ship.png");
-
         this.onKeyDown(this.processControls);
     }
 
@@ -27,34 +31,40 @@ class PlayerShip extends HelixSprite
     private function processControls(keys:Array<FlxKey>):Void
     {
         this.resetAcceleration();
-        if (keys.has(FlxKey.LEFT) || keys.has(FlxKey.A)) { 
-            this.rotateLeft(); 
+
+        if (keys.has(FlxKey.LEFT) || keys.has(FlxKey.A))
+        { 
+            this.angularVelocity = -ROTATION_VELOCITY;            
         }
-        if (keys.has(FlxKey.RIGHT) || keys.has(FlxKey.D)) { 
-            this.rotateRight(); 
+        else if (keys.has(FlxKey.RIGHT) || keys.has(FlxKey.D))
+        { 
+            this.angularVelocity = ROTATION_VELOCITY;
         }
-        if (keys.has(FlxKey.UP) || keys.has(FlxKey.W)) { 
-            this.accelerateForward(); 
+        else if (keys.has(FlxKey.UP) || keys.has(FlxKey.W))
+        { 
+            this.accelerateForward(ACCELERATION); 
+        }
+        else if (keys.has(FlxKey.DOWN) || keys.has(FlxKey.S))
+        {
+            this.accelerateForward(-ACCELERATION * DECELERATION_MULTIPLIER); 
         }
     }
 
-    private function rotateLeft():Void
+    private function accelerateForward(acceleration:Int):Void
     {
-        this.angularVelocity -= 200;
-    }
-    private function rotateRight():Void
-    {
-        this.angularVelocity += 200;
-    }
-    private function accelerateForward():Void
-    {
-        this.acceleration.set(0, -90); 
+        this.acceleration.set(0, -acceleration); 
         this.acceleration.rotate(FlxPoint.weak(0, 0), this.angle);
     }
 
     override public function update(elapsedSeconds:Float):Void
     {
         super.update(elapsedSeconds);
+
+        if (FlxG.keys.getIsDown().length == 0)
+        {
+            this.angularVelocity = 0;
+        }
+        
         FlxSpriteUtil.screenWrap(this);
     }
 }
