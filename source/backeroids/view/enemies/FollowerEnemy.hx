2c9eb79e08d5ea9990b5d2b4511d3edea29d6dde
NegativeScript <ala2aldeen913@gmail.com>
2017-11-17 09:11:20 +0300

    refactored Tank and Kamikaze to subclass FollowerEnemy

---
+++ source/backeroids/view/enemies/FollowerEnemy.hx
@@ -0,0 +1,46 @@
+package backeroids.view.enemies;
+
+import backeroids.view.enemies.AbstractEnemy;
+import backeroids.view.PlayerShip;
+import flixel.FlxG;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRandom;
+import helix.data.Config;
+
+class FollowerEnemy extends AbstractEnemy
+{
+    private static var random = new FlxRandom();
+    private var player:PlayerShip;
+    private var baseVelocity:Int;
+
+    private function new(player:PlayerShip, colorDetails)
+    {
+        super(null, colorDetails);
+        this.elasticity = Config.get("enemies").elasticity;
+        this.player = player;
+
+        if (random.bool() == true)
+        {
+            // Place beside the stage
+            this.x = random.bool() == true ? FlxG.width : -this.width;
+            this.y = random.int(0, Std.int(FlxG.height - this.height));
+        }
+        else
+        {
+            // Place above/below the stage
+            this.x = random.int(0, Std.int(FlxG.width - this.width));
+            this.y = random.bool() == true ? FlxG.height : -this.height;
+        }
+    }
+
+    override public function update(elapsedSeconds:Float):Void
+    {
+        super.update(elapsedSeconds);
+        var me = FlxPoint.weak(this.x, this.y);
+        var player = FlxPoint.weak(this.player.x, this.player.y);
+        
+        this.angle = me.angleBetween(player);
+        this.velocity.set(0, -this.baseVelocity);
+        this.velocity.rotate(FlxPoint.weak(0, 0), this.angle); 
+    }
+}
--- source/backeroids/view/enemies/Kamikaze.hx
+++ source/backeroids/view/enemies/Kamikaze.hx
@@ -1,39 +1,17 @@
 package backeroids.view.enemies;
 
-import backeroids.view.enemies.AbstractEnemy;
+import backeroids.view.enemies.FollowerEnemy;
 import backeroids.view.PlayerShip;
-import flixel.FlxG;
-import flixel.math.FlxPoint;
-import flixel.math.FlxRandom;
 import helix.data.Config;
 
-class Kamikaze extends AbstractEnemy
+class Kamikaze extends FollowerEnemy
 {
-    private static var random = new FlxRandom();
-    private var player:PlayerShip;
-    private var baseVelocity:Int;
-
     public function new(player:PlayerShip)
     {
-        super(null, {width: 30, height: 30, colour: 0xFFc10000 });
+        super(player, {width: 30, height: 30, colour: 0xFFc10000 });
 
-        this.elasticity = Config.get("enemies").elasticity;
         var config:Dynamic = Config.get("enemies").kamikaze;
-
         this.baseVelocity = config.velocity;
         this.health = config.health;
-
-        this.player = player;
-    }
-
-    override public function update(elapsedSeconds:Float):Void
-    {
-        super.update(elapsedSeconds);
-        var me = FlxPoint.weak(this.x, this.y);
-        var player = FlxPoint.weak(this.player.x, this.player.y);
-        
-        this.angle = me.angleBetween(player);
-        this.velocity.set(0, -this.baseVelocity);
-        this.velocity.rotate(FlxPoint.weak(0, 0), this.angle); 
     }
 }
