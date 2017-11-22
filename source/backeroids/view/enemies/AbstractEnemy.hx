f842fc468984092ab4f83223eacfd2952b07dbec
nightblade9 <nightbladecodes@gmail.com>
2017-11-22 08:54:59 -0500

    Kamikaze doesn't aimbot any more Moved some common stuff into AbstractEnemy Reorganized states into package

--- source/backeroids/view/enemies/AbstractEnemy.hx
+++ source/backeroids/view/enemies/AbstractEnemy.hx
@@ -1,8 +1,32 @@
 package backeroids.view.enemies;
 
 import flixel.FlxG;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRandom;
 import helix.core.HelixSprite;
+import helix.data.Config;
 
 class AbstractEnemy extends HelixSprite
 {
+    private static var random = new FlxRandom();
+
+    public function new(filename, colorDetails)
+    {
+        super(filename, colorDetails);
+        this.elasticity = Config.get("enemies").elasticity;
+
+        if (random.bool() == true)
+        {
+            // Place beside the stage
+            this.x = random.bool() == true ? FlxG.width : -this.width;
+            this.y = random.int(0, Std.int(FlxG.height - this.height));
+        }
+        else
+        {
+            // Place above/below the stage
+            this.x = random.int(0, Std.int(FlxG.width - this.width));
+            this.y = random.bool() == true ? FlxG.height : -this.height;
+        }
+
+    }
 }
