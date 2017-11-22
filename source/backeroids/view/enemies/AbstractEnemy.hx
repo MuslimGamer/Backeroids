6917e609a2e8bc116835b93bec6704925f1b1e02
NegativeScript <ala2aldeen913@gmail.com>
2017-11-22 22:00:42 +0300

    possible fix for #26

--- source/backeroids/view/enemies/AbstractEnemy.hx
+++ source/backeroids/view/enemies/AbstractEnemy.hx
@@ -1,8 +1,29 @@
 package backeroids.view.enemies;
 
-import flixel.FlxG;
 import helix.core.HelixSprite;
+import helix.data.Config;
 
 class AbstractEnemy extends HelixSprite
 {
+    private var hasAppearedOnscreen:Bool = false;
+
+    private function new(filename, colorDetails)
+    {
+        super(filename, colorDetails);
+        this.elasticity = Config.get("enemies").elasticity;
+    }
+
+    override public function update(elapsedSeconds:Float):Void
+    {
+        super.update(elapsedSeconds);
+        if (this.hasAppearedOnscreen && !this.isOnScreen())
+        {
+            this.kill();
+        }
+
+        if (this.isOnScreen())
+        {
+            this.hasAppearedOnscreen = true;
+        }
+    }
 }
