08199c8a7b5825039ac508cd513d908b8fa2df86
NegativeScript <ala2aldeen913@gmail.com>
2017-11-29 21:25:28 +0300

    Splash screen audio

--- source/backeroids/states/SplashState.hx
+++ source/backeroids/states/SplashState.hx
@@ -2,6 +2,7 @@ package backeroids.states;
 
 import backeroids.SoundManager;
 import flixel.FlxG;
+import flixel.system.FlxSound;
 import helix.core.HelixSprite;
 using helix.core.HelixSpriteFluentApi;
 import helix.core.HelixState;
@@ -18,15 +19,14 @@ class SplashState extends HelixState
     private var onComplete:Void->Void;
     private var isDone:Bool = false;
     private var imageFile:String;
-    private var audioFile:String;
+    private var audioFile:FlxSound;
 
-    public function new(imageFile:String, audioFile:String, onComplete:Void->Void)
+    public function new(imageFile:String, audioFile:FlxSound, onComplete:Void->Void)
     {
         super();
         this.imageFile = imageFile;
         this.audioFile = audioFile;
         this.onComplete = onComplete;
-        // TODO: play audio
     }
 
     override public function create()
@@ -38,6 +38,7 @@ class SplashState extends HelixState
         image.y = (FlxG.height - image.height) / 2;
         image.alpha = 0;
         start = GameTime.now();
+        this.audioFile.play();
     }
 
     override public function update(elapsedSeconds:Float):Void
