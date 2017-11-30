6ce507c06040d2981ebd3a1d53fda266db13cfa9
NegativeScript <ala2aldeen913@gmail.com>
2017-11-30 16:17:56 +0300

    shield only recharges when deactivated

--- source/backeroids/view/Shield.hx
+++ source/backeroids/view/Shield.hx
@@ -33,7 +33,7 @@ class Shield extends HelixSprite
         var now = GameTime.now();
         if (now.elapsedSeconds - this.lastRecharge.elapsedSeconds > Config.get('ship').shield.secondsPerRecharge)
         {
-            if (this.shieldHealth < this.totalShieldHealth)
+            if (this.shieldHealth < this.totalShieldHealth && !this.isActivated)
             {
                 this.shieldHealth += 1;
                 if (this.indicatorCallback != null)
