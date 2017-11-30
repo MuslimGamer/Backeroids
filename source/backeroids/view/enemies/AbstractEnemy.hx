0e2f90e3e372cfe8bcfd0727f95d35404ca4b8f6
NegativeScript <ala2aldeen913@gmail.com>
2017-11-30 16:31:21 +0300

    enemies now wrap, shield is tougher

--- source/backeroids/view/enemies/AbstractEnemy.hx
+++ source/backeroids/view/enemies/AbstractEnemy.hx
@@ -4,12 +4,12 @@ import backeroids.SoundManager;
 import flixel.FlxG;
 import flixel.math.FlxPoint;
 import flixel.math.FlxRandom;
+import flixel.util.FlxSpriteUtil;
 import helix.core.HelixSprite;
 import helix.data.Config;
 
 class AbstractEnemy extends HelixSprite
 {
-    private var hasAppearedOnscreen:Bool = false;
     private static var random = new FlxRandom();
 
     private function new(?filename, ?colorDetails)
@@ -34,15 +34,7 @@ class AbstractEnemy extends HelixSprite
     override public function update(elapsedSeconds:Float):Void
     {
         super.update(elapsedSeconds);
-        if (this.hasAppearedOnscreen && !this.isOnScreen())
-        {
-            this.kill();
-        }
-
-        if (this.isOnScreen())
-        {
-            this.hasAppearedOnscreen = true;
-        }
+        FlxSpriteUtil.screenWrap(this);
     }
 
     public function damage():Void
